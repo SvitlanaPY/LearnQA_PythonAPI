d1cd6e62b8f315f91e74504c28691056b7ead4ce
Svitlana Pushchalo <svitlana8@gmail.com>
2023-12-10 21:30:18 +0200

    New LearnQA_PythonAPI Project

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+
+response = requests.get("https://playground.learnqa.ru/api/hello")
+print(response.text)
+
+response2 = requests.get("https://playground.learnqa.ru/api/get_text")
+print(response2.text)
+
+print("Hello from Svitlana")
