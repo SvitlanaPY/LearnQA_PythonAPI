7f39fc07a20e64a1486d7f48e3e574747ab73e6c
Svitlana Pushchalo <svitlana8@gmail.com>
2024-01-01 22:24:20 +0200

    "Json parsing;;; json_loads;;; response_json"

--- CheckType.py
+++ CheckType.py
@@ -3,9 +3,9 @@ import requests
 params = ['1', '2', '3', '4']
 
 response = requests.get("https://playground.learnqa.ru/api/check_type", params={"param1": "value1"})
-# response = requests.post("https://playground.learnqa.ru/api/check_type")
-# response = requests.put("https://playground.learnqa.ru/api/check_type")
-# response = requests.delete("https://playground.learnqa.ru/api/check_type")
+# response = requests.post("https://playground.learnqa.ru/api/check_type", data={"param1": "value1"})
+# response = requests.put("https://playground.learnqa.ru/api/check_type", data={"param1": "value1"})
+# response = requests.delete("https://playground.learnqa.ru/api/check_type", data={"param1": "value1"})
 
 print(response.text)
 print(response.url)
