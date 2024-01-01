d26f9abcc8cda04663a972515ec2066cd8836575
Svitlana Pushchalo <svitlana8@gmail.com>
2024-01-01 22:50:02 +0200

    "Json parsing;;; json_loads;;; response_json"

---
+++ HW_2.7.py
@@ -0,0 +1,52 @@
+import requests
+
+# --- #1 ---
+# response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type")
+# print("GET:", response.text)
+# print("GET:", response.url)
+# print(f"GET: {response.status_code}: {response.reason}")
+#
+# response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type")
+# print("POST:", response.text)
+# print("POST:", response.url)
+# print(f"POST: {response.status_code}: {response.reason}")
+#
+# response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type")
+# print("PUT:", response.text)
+# print("PUT:", response.url)
+# print(f"PUT: {response.status_code}: {response.reason}")
+#
+# response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type")
+# print("DEL:", response.text)
+# print("DEL:", response.url)
+# print(f"DEL: {response.status_code}: {response.reason}")
+#
+# --- #2 ---
+# response = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type")
+# print("HEAD:", response.text)
+# print("HEAD:", response.url)
+# print(f"HEAD: {response.status_code}: {response.reason}")
+
+# --- #3 ---
+# response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": "GET"})
+# print("GET:", response.text)
+# print("GET:", response.url)
+# print(f"GET: {response.status_code}: {response.reason}")
+#
+# response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": "POST"})
+# print("GET:", response.text)
+# print("GET:", response.url)
+# print(f"GET: {response.status_code}: {response.reason}")
+#
+# response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": "PUT"})
+# print("GET:", response.text)
+# print("GET:", response.url)
+# print(f"GET: {response.status_code}: {response.reason}")
+#
+# response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": "DELETE"})
+# print("GET:", response.text)
+# print("GET:", response.url)
+# print(f"GET: {response.status_code}: {response.reason}")
+
+# --- #4 ---
+
