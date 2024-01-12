73d5e00f85fe73cf1968b138754a8b2234ab09da
Svitlana Pushchalo <svitlana8@gmail.com>
2024-01-13 00:02:31 +0200

    "Module_#3"

--- test_examples.py
+++ test_examples.py
@@ -2,4 +2,11 @@ class TestExample:
     def test_check_math(self):
         a = 5
         b = 9
-        assert a+b == 14
+        expected_sum = 14
+        assert a + b == expected_sum, f"Sum of variables a and b is not equal {expected_sum}"
+
+    def test_check_math2(self):
+        a = 5
+        b = 11
+        expected_sum = 14
+        assert a + b == expected_sum, f"Sum of variables a and b is not equal {expected_sum}"
