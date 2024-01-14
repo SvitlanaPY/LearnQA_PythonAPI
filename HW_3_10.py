f808230b3cbad2d0fc5e11a003789f609b059701
Svitlana Pushchalo <svitlana8@gmail.com>
2024-01-14 15:09:16 +0200

    "Module_#3"

---
+++ HW_3_10.py
@@ -0,0 +1,19 @@
+# Ex10: Тест на короткую фразу
+#
+# В рамках этой задачи с помощью pytest необходимо написать тест, который просит ввести в консоли любую фразу короче
+# 15 символов. А затем с помощью assert проверяет, что фраза действительно короче 15 символов.
+#
+# Чтобы в переменную получить значение, введенное из консоли, необходимо написать вот такой код:
+# phrase = input("Set a phrase: ")
+#
+# Внимание, чтобы pytest не игнорировал команду ввода с клавиатуры, запускать тест нужно с ключиком "-s":
+# python -m pytest -s my_test.py
+
+import pytest
+class TestPhrase:
+    def test_check_phrase_length(self):
+        phrase = input("Set a phrase: ")
+        assert len(phrase) < 15, f"Length of the phrase is more than 15"
+
+if __name__ == '__main__':
+    pytest.main(['--capture=no'])
