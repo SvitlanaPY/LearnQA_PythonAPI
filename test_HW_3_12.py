0a621b6dd87cf5cc1e68fac7cff92445d0452481
Svitlana Pushchalo <svitlana8@gmail.com>
2024-01-15 16:10:26 +0200

    "Module_#3"

--- test_HW_3_12.py
+++ test_HW_3_12.py
@@ -7,3 +7,20 @@
 #
 # Чтобы pytest не игнорировал print() необходимо использовать ключик "-s": python -m pytest -s my_test.py
 
+import requests
+
+class TestHeaders:
+
+    def test_check_headers_pass(self):
+        response = requests.get("https://playground.learnqa.ru/api/homework_header")
+        # print(response.text)
+        assert response.status_code == 200, 'Wrong status code'
+
+        print(response.headers)
+        headers_ = dict(response.headers)
+        assert 'Connection' in headers_, "There is no header_name 'Connection' in the headers"
+
+        actual_ConnectionHeader_value = headers_['Connection']  # actual_cookie_value = response.cookies.get('HomeWork')
+        expected_ConnectionHeader_value = 'keep-alive'
+        assert actual_ConnectionHeader_value == expected_ConnectionHeader_value, 'Actual ConnectionHeader_value in the headers is NOT correct'
+
