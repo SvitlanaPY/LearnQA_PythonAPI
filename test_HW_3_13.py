ab977516809f7c8c8ea1ad9aaaaa82f5b693c14a
Svitlana Pushchalo <svitlana8@gmail.com>
2024-02-25 22:45:58 +0200

    "Module_#3"

--- test_HW_3_13.py
+++ test_HW_3_13.py
@@ -34,14 +34,14 @@ import requests
 
 class TestUserAgent:
     ParametersList = [
-        ("Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"),
-        ("Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1"),
-        ("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"),
-        ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0"),
-        ("Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1")
+        ("Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30","Mobile","No","Android"),
+        ("Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1","Mobile","Chrome","iOS"),
+        ("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)","Googlebot","Unknown","Unknown"),
+        ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0","Web","Chrome","No"),
+        ("Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1","Mobile","No","iPhone")
     ]
-    @pytest.mark.parametrize('user_agent', ParametersList)
-    def test_user_agent(self, user_agent):
+    @pytest.mark.parametrize('user_agent, expected_platform, expected_browser, expected_device', ParametersList)
+    def test_user_agent(self, user_agent, expected_platform, expected_browser, expected_device):
         url = 'https://playground.learnqa.ru/ajax/api/user_agent_check'
         response = requests.get(url, headers={"User-Agent": user_agent})
         assert response.status_code == 200, 'Wrong status code'
@@ -51,3 +51,9 @@ class TestUserAgent:
         assert 'browser' in response_dict, "There is no key 'browser' in the response"
         assert 'device' in response_dict, "There is no key 'device' in the response"
 
+        actual_platform = response_dict['platform']
+        assert actual_platform == expected_platform, "Actual platform in the response is NOT correct"
+        actual_browser = response_dict['browser']
+        assert actual_browser == expected_browser, "Actual browser in the response is NOT correct"
+        actual_device = response_dict['device']
+        assert actual_device == expected_device, "Actual device in the response is NOT correct"
