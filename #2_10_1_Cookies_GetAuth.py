784c75a38545843a720ca1e2a763952ec1edeb50
Svitlana Pushchalo <svitlana8@gmail.com>
2025-05-02 17:28:20 +0300

    "Module2"

--- #2_10_1_Cookies_GetAuth.py
+++ #2_10_1_Cookies_GetAuth.py
@@ -1,6 +1,22 @@
+# Cookie - це спеціальні файли, їх створює клієнт(мобільний додаток чи браузер) на основі відповіді від сервера.
+# ці файли мають термін дії, після чого вони видаляються клієнтом.
+# в кожного Cookie є: імя, значення, домен.
+# Щоразу, коли клієнт створює http-запит на сервер, він додає всі cookies, що стосуються домену, на який робиться цей запит
+# Cookie використовуються для різних цілей, і сама популярна з них - авторизація.
+# Коли користувач приходить на сайт не будучи авторизованим, то сайт просить його ввести login та password.
+# Ці дані із http-запитом ідуть на сервер. Сервер їх перевіряє, і якщо вони вірні, то сервер у відповідь присилає cookies, із спеціальним іменем та значенням, які є тільки у цього користувача.
+# і наступного разу, коли клієнт користувача буде робити http-запит на сервер, то він додасть значення цього cookie і сервер впізнає користувача.
+# Cookie ще використовуються, щоб запам"ятовувати якусь інформацію про користувача: наприклад, відрізняти тих, хто вже був на якійсь сторінці, від тих, хто ще не був,
+# відслідковувати дії користувача на сайті і т.д.
+# бібліотека requests може як відправляти Cookie так і отримувати Cookie.
+# Cookie у змінній response зберігається у вигляді об"єкту. І щоб побачити значення Cookie, спершу треба його привести до вигляду словника.
+# header 'Set-Cookie' - у response-і від сервера:
+# значення cookie, дата дії cookie, домен, до якого належить cookie, path- це частина url без домену, для яких це cookie треба передавати
+# (якщо path=/ то передавати треба для усіх путей), прапорець HttpOnly- щоб cookie була ахощена від XSS та CSRF атак.
+
 import requests
 
-# ex.#1
+# ex.#1 correct pwd
 payload = {"login": "secret_login", "password": "secret_pass"}
 response = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload)
 
@@ -8,8 +24,21 @@ print('ex.#1')
 print(response.text)
 print(response.status_code)
 print(response.cookies)
-print(dict(response.cookies))
+print(dict(response.cookies))     # {'auth_cookie': '322410'}
 print(response.headers)
+# response.headers:
+# {
+#   'Date': 'Fri, 02 May 2025 13:04:38 GMT',
+#   'Content-Type': 'text/html; charset=utf-8',
+#   'Content-Length': '0',
+#   'Connection': 'keep-alive',
+#   'Keep-Alive': 'timeout=10',
+#   'Server': 'Apache',
+#   'Set-Cookie': 'auth_cookie=509982; expires=Mon, 02-Jun-2025 13:04:38 GMT; Max-Age=2678400; path=/; domain=playground.learnqa.ru; HttpOnly',
+#   'Cache-Control': 'max-age=0',
+#   'Expires': 'Fri, 02 May 2025 13:04:38 GMT'
+# }
+
 
 
 # ex.#2 incorrect pwd
--- #2_10_2_Cookies_CheckAuth.py
+++ #2_10_2_Cookies_CheckAuth.py
@@ -1,28 +1,28 @@
 import requests
 
-# case#1: when login and password are correct:
-# payload = {"login": "secret_login", "password": "secret_pass"}
-# response11 = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload)
-#
-# cookie_value = response1.cookies.get('auth_cookie')
-# # print('cookie_value= ', cookie_value)
-# cookies = {'auth_cookie': cookie_value}
-# response22 = requests.get("https://playground.learnqa.ru/api/check_auth_cookie", cookies=cookies)
-#
-# print(response22.text)
+# case#1: when login and password are CORRECT:
+payload_correct = {"login": "secret_login", "password": "secret_pass"}
+response11 = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload_correct)
+print(dict(response11.cookies))
+cookie_value_correct = response11.cookies.get('auth_cookie')    # за назвою cookie (а саме 'auth_cookie') отримуємо значення cookie і кладемо це значення у змінну cookie_value_correct
+print('cookie_value_correct= ', cookie_value_correct)
+cookie_correct = {'auth_cookie': cookie_value_correct}
+# отримане значення cookie передаємо в get-запиті у cookies=
+response22 = requests.get("https://playground.learnqa.ru/api/check_auth_cookie", cookies=cookie_correct)
+print(response22.text)    # You are authorized (when password is correct)
 
-# case#2: when login and password are NOT correct (then cookies are not sent by the server, cookie value in None):
-# payload = {"login": "secret_login", "password": "secret_pass"}
-payload = {"login": "secret_login", "password": "secret_pass12345"}
-response1 = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload)
-# print(response1.cookies)
-# print(dict(response1.cookies))
-cookie_value = response1.cookies.get('auth_cookie')
-# print('cookie_value= ', cookie_value)
-cookies_ = {}
-if cookie_value is not None:
-    cookies_.update({'auth_cookie': cookie_value})
 
-response2 = requests.get("https://playground.learnqa.ru/api/check_auth_cookie", cookies=cookies_)
+# case#2: when login and password are INCORRECT (тоді сервер не присилає cookie(маємо {}), і cookie value буде None в цьому випадку)
+# коли значення cookie є None, то передавати таке значення(None) дальше немає сенсу
+payload_incorrect = {"login": "secret_login", "password": "secret_pass12345"}
+response1 = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload_incorrect)
+print(dict(response1.cookies))
+cookie_value_ = response1.cookies.get('auth_cookie')   # якщо викликати метод get з ключем, якого неіснує(i.e. 'auth_cookie'), то повернеться значення None
+print('cookie_value_= ', cookie_value_)   # cookie_value_=  None
+cookie_ = {}
+if cookie_value_ is not None:
+    cookie_.update({'auth_cookie': cookie_value_})
 
-print(response2.text)   # You are authorized (when password is correct); You are NOT authorized (when password is incorrect)
+response2 = requests.get("https://playground.learnqa.ru/api/check_auth_cookie", cookies=cookie_)
+print(response2.text)   # You are NOT authorized (when password is incorrect)
+
