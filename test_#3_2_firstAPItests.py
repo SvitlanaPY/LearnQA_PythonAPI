df644a47d74ec8bb7cfd34cd97a4d4ec24dc0cfd
Svitlana Pushchalo <svitlana8@gmail.com>
2025-05-05 22:46:36 +0300

    "Module3"

--- test_#3_2_firstAPItests.py
+++ test_#3_2_firstAPItests.py
@@ -4,8 +4,8 @@ import requests
 class TestFirstAPI:
     def test_hello_call_pass(self):
         url = "https://playground.learnqa.ru/api/hello"
-        name = "Svitlana"
-        data = {'name': name}
+        name_ = "Svitlana"
+        data = {'name': name_}    # де 'name' - є параметром даного методу(endpoint-а),
 
         response = requests.get(url, params=data)
         assert response.status_code == 200, "Wrong status code"
@@ -13,13 +13,13 @@ class TestFirstAPI:
         response_dict = response.json()
         assert "answer" in response_dict, "There is no key 'answer' in the response"
 
-        expected_response_text = f"Hello, {name}"
+        expected_response_text = f"Hello, {name_}"
         actual_response_text = response_dict["answer"]
         assert actual_response_text == expected_response_text, "Actual text in the response is NOT correct"
 
     def test_hello_call_fail(self):
         url = "https://playground.learnqa.ru/api/hello"
-        name = "Svitlana"
+        name_ = "Svitlana"
 
         response = requests.get(url)
         assert response.status_code == 200, "Wrong status code"
@@ -27,6 +27,12 @@ class TestFirstAPI:
         response_dict = response.json()
         assert "answer" in response_dict, "There is no key 'answer' in the response"
 
-        expected_response_text = f"Hello, {name}"
+        expected_response_text = f"Hello, {name_}"
         actual_response_text = response_dict['answer']
         assert actual_response_text == expected_response_text, "Actual text in the response is NOT correct"
+
+#        AssertionError: Actual text in the response is NOT correct
+#        assert 'Hello, someone' == 'Hello, Svitlana'
+#          - Hello, Svitlana   (те, що ми очікуємо)
+#          + Hello, someone    (те, що на нам насправді прийшло)
+
--- test_#3_3_parametrizedAPItest.py
+++ test_#3_3_parametrizedAPItest.py
@@ -11,15 +11,25 @@ class TestFirstAPI:
     @pytest.mark.parametrize('name_', names)
     def test_hello_call(self, name_):
         url = 'https://playground.learnqa.ru/api/hello'
-        data = {'name': name_}
+        data = {'name': name_}    # де 'name' - є параметром даного методу(endpoint-а), а 'name_' - змінна, через яку пропихаються дані, які зберігаються в змінній 'names'
 
         response = requests.get(url, params=data)   # response = requests.get(url, params={'name': name_})
         assert response.status_code == 200, 'Wrong status code'
+
         response_dict = response.json()
         assert 'answer' in response_dict, "There is no key 'answer' in the response"
-        actual_response_text = response_dict['answer']
+
+# для тесту з порожнім іменем формується строка з порожнім іменем ('Hello, '), а насправді нам приходить строка з дефолтним значеням someone, тобто нам приходить 'Hello, someone'
+# тому в наш код потрібно додати перевірку:
         if name_ == "":   # if len(name_) == 0:
             expected_response_text = f"Hello, someone"
         else:
             expected_response_text = f"Hello, {name_}"
+        actual_response_text = response_dict['answer']
         assert actual_response_text == expected_response_text, "Actual text in the response is NOT correct"
+
+
+# в @pytest.mark.parametrize ми вказуємо ім"я змінної ('name_'), в яку pytest буде передавати дані,
+# а через кому вказуємо змінну, в якій зараз ці дані зберігаються('names').
+# після self в назві тесту ми вказуємо саме ту змінну ('name_'), в яку pytest передавав дані
+
