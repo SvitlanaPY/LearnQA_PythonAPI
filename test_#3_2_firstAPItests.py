c4175f8ba201aee23f87f44915081e2a35ec4566
Svitlana Pushchalo <svitlana8@gmail.com>
2025-05-05 18:46:05 +0300

    "Module3"

--- test_#3_2_firstAPItests.py
+++ test_#3_2_firstAPItests.py
@@ -3,29 +3,29 @@ import requests
 
 class TestFirstAPI:
     def test_hello_call_pass(self):
-        url = 'https://playground.learnqa.ru/api/hello'
-        name = 'Svitlana'
+        url = "https://playground.learnqa.ru/api/hello"
+        name = "Svitlana"
         data = {'name': name}
 
         response = requests.get(url, params=data)
-        assert response.status_code == 200, 'Wrong status code'
+        assert response.status_code == 200, "Wrong status code"
 
         response_dict = response.json()
-        assert 'answer' in response_dict, "There is no key 'answer' in the response"
+        assert "answer" in response_dict, "There is no key 'answer' in the response"
 
         expected_response_text = f"Hello, {name}"
-        actual_response_text = response_dict['answer']
+        actual_response_text = response_dict["answer"]
         assert actual_response_text == expected_response_text, "Actual text in the response is NOT correct"
 
     def test_hello_call_fail(self):
-        url = 'https://playground.learnqa.ru/api/hello'
-        name = 'Svitlana'
+        url = "https://playground.learnqa.ru/api/hello"
+        name = "Svitlana"
 
         response = requests.get(url)
-        assert response.status_code == 200, 'Wrong status code'
+        assert response.status_code == 200, "Wrong status code"
 
         response_dict = response.json()
-        assert 'answer' in response_dict, "There is no key 'answer' in the response"
+        assert "answer" in response_dict, "There is no key 'answer' in the response"
 
         expected_response_text = f"Hello, {name}"
         actual_response_text = response_dict['answer']
