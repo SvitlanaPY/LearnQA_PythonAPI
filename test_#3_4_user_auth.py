03ab8848f2f42205dd911dc2bf118b9ddd7efd3f
Svitlana Pushchalo <svitlana8@gmail.com>
2025-05-05 23:34:45 +0300

    "Module3"

--- test_#3_4_user_auth.py
+++ test_#3_4_user_auth.py
@@ -30,16 +30,23 @@ class TestUserAuth:
 
         response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=auth_data)
         assert response1.status_code == 200, 'Wrong status code'
+
         assert "auth_sid" in response1.cookies, "There is no auth cookie in the response1"
         auth_sid = response1.cookies.get("auth_sid")
+
         assert "x-csrf-token" in response1.headers, "There is no CSRF token header in the response1"
         token = response1.headers.get("x-csrf-token")
+
         assert "user_id" in response1.json(), "There is no user id in the response1"
-        user_id_from_auth_method = response1.json().get("user_id")   # user_id_from_auth_method = response1.json()["user_id"]
+        user_id_from_auth_method = response1.json().get("user_id")   # або user_id_from_auth_method = response1.json()["user_id"], але
+        # якщо такого ключа "user_id" неіснує, то тест впаде у помилку, тоді як при застусуванні get("user_id") до response1.json() - повертається значення None
+
 
         response2 = requests.get("https://playground.learnqa.ru/api/user/auth", headers={"x-csrf-token": token}, cookies={"auth_sid": auth_sid})
         assert response2.status_code == 200, 'Wrong status code'
+
         assert "user_id" in response2.json(), "There is no user id in the response2"
-        user_id_from_check_method = response2.json()["user_id"]
+        user_id_from_check_method = response2.json()["user_id"]    # або response2.json().get("user_id")
+
         assert user_id_from_auth_method == user_id_from_check_method, "User id from auth method is not equal to user id from check method"
 
