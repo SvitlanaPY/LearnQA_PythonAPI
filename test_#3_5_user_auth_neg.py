092552eccb84cc2a30229a8b505c1bbd9cbd4aa6
Svitlana Pushchalo <svitlana8@gmail.com>
2025-05-16 19:31:31 +0300

    "Module3"

--- test_#3_5_user_auth_neg.py
+++ test_#3_5_user_auth_neg.py
@@ -35,27 +35,27 @@ class TestUserAuth:
 
         response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=auth_data)
         assert response1.status_code == 200, 'Wrong status code'
+
         assert "auth_sid" in response1.cookies, "There is no auth cookie in the response1"
         auth_sid = response1.cookies.get("auth_sid")
+
         assert "x-csrf-token" in response1.headers, "There is no CSRF token header in the response1"
         token = response1.headers.get("x-csrf-token")
-        assert "user_id" in response1.json(), "There is no user id in the response1"
+
+        # assert "user_id" in response1.json(), "There is no user id in the response1"
         # user_id_from_auth_method = response1.json().get("user_id") -
-        # "user_id" нам не потрібне, бо при подальших перевірках ми не будемо його використовувати, тому ми його не запамятовуємо у змінну.
+        # "user_id" нам не потрібне, бо при подальших перевірках ми не будемо його використовувати, тому ми його не запамятовуємо у змінну
 
 
         if condition == "no_cookie":
-            response2 = requests.get(
-                "https://playground.learnqa.ru/api/user/auth",
-                headers={"x-csrf-token": token}
-            )
+            response2 = requests.get("https://playground.learnqa.ru/api/user/auth", headers={"x-csrf-token": token})
         else:
-            response2 = requests.get(
-                "https://playground.learnqa.ru/api/user/auth",
-                cookies={"auth_sid": auth_sid}
-            )
+            response2 = requests.get("https://playground.learnqa.ru/api/user/auth", cookies={"auth_sid": auth_sid})
 
         assert response2.status_code == 200, 'Wrong status code'
+
         assert "user_id" in response2.json(), "There is no user id in the response2"
-        user_id_from_check_method = response2.json()["user_id"]
+        user_id_from_check_method = response2.json().get("user_id")    # = response2.json()["user_id"]
+
         assert user_id_from_check_method == 0, f"User is authorized with {condition}"
+
--- test_#3_5_user_auth_posit_neg.py
+++ test_#3_5_user_auth_posit_neg.py
@@ -64,23 +64,17 @@ class TestUserAuth:
         assert response1.status_code == 200, 'Wrong status code'
         assert "auth_sid" in response1.cookies, "There is no auth cookie in the response1"
         assert "x-csrf-token" in response1.headers, "There is no CSRF token header in the response1"
-        assert "user_id" in response1.json(), "There is no user id in the response1"
+        # assert "user_id" in response1.json(), "There is no user id in the response1"
 
         auth_sid = response1.cookies.get("auth_sid")
         token = response1.headers.get("x-csrf-token")
 
         if condition == "no_cookie":
-            response2 = requests.get(
-                "https://playground.learnqa.ru/api/user/auth",
-                headers={"x-csrf-token": token}
-            )
+            response2 = requests.get("https://playground.learnqa.ru/api/user/auth", headers={"x-csrf-token": token})
         else:
-            response2 = requests.get(
-                "https://playground.learnqa.ru/api/user/auth",
-                cookies={"auth_sid": auth_sid}
-            )
+            response2 = requests.get("https://playground.learnqa.ru/api/user/auth", cookies={"auth_sid": auth_sid})
 
         assert response2.status_code == 200, 'Wrong status code'
         assert "user_id" in response2.json(), "There is no user id in the response2"
-        user_id_from_check_method = response2.json()["user_id"]
+        user_id_from_check_method = response2.json().get("user_id")
         assert user_id_from_check_method == 0, f"User is authorized with {condition}"
